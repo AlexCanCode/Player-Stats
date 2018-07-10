46a3208b5be9673cd02c686e167deddec4ed8b9a
Alex Richards <arichards@go-iei.com>
2018-07-10 16:06:52 -0600

    Finished scraping feature, gets all desired data points for 2018 season

--- webScraper.py
+++ webScraper.py
@@ -1,3 +1,4 @@
+import csv
 import requests
 from bs4 import BeautifulSoup
 
@@ -6,5 +7,19 @@ response = requests.get(url)
 html = response.content
 
 soup = BeautifulSoup(html, "html.parser")
-table = soup.find("table")
-print (table.prettify())
+table = soup.find('tbody')
+
+list_of_rows = []
+for row in table.findAll('tr'):
+	list_of_cells = []
+	for cell in row.findAll('td'):
+		text = cell.text
+		list_of_cells.append(text)
+	list_of_rows.append(list_of_cells)
+
+clean_list_of_rows = filter(None, list_of_rows)
+
+outfile = open("./players.csv", "w", newline='')
+writer = csv.writer(outfile)
+writer.writerow(["Player", "Pos", "Age", "Tm", "G", "GS", "MP", "FG", "FGA", "FG%", "3P", "3PA", "3P%", "2P", "2PA", "2P%", "eFG%", "FT", "FTA", "FT%", "ORB", "DRB", "TRB", "AST", "STL", "BLK", "TOV", "PF", "PS/G"])
+writer.writerows(clean_list_of_rows)
