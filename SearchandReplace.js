147052ed79e8041200ac1990d03456640a4423e4
Alex Richards <AlexCanCode18@gmail.com>
2018-09-03 11:24:54 -0600

    Implemented logic to search nodes based on an array

--- SearchandReplace.js
+++ SearchandReplace.js
@@ -9,29 +9,18 @@ function walkTheDOM(node, func) {
 	}
 }
 
-const players = ["jayson tatum", "joe ingles", "lance stephenson", "tim duncan", "michael jordan", "derrick rose", "javale mcgee", "harry giles", "paul pierce", "dwight howard", "kevin durant", "shaun livingston", "nikola jokic", "deandre jordan", "luol deng", "goran dragic", "steph curry", "wayne ellington"]
+const players = ["jayson tatum", "joe ingles", "lance stephenson", "tim duncan", "michael jordan", "derrick rose", "javale mcgee", "harry giles", "paul pierce", "dwight howard", "kevin durant", "shaun livingston", "nikola jokic", "deandre jordan", "luol deng", "goran dragic", "steph curry", "wayne ellington"] //NEXT: Generate this list from hash result 
 
 const nodeArray = []
 
 walkTheDOM(document.body, function(node) {
-	let found = false;
 	if(node.children){
 		if(node.children.length === 0){
-			for(i = 0; i < (players.length - 1); i++){
-				if(found){
-					break
-				}
-				else if(node.textContent.toLowerCase().includes(players[i]))
-				console.log(node, players[i])
-				found = true;
+			if(new RegExp(players.join("|"), "i").test(node.textContent)) {
+					nodeArray.push(node);
 			}
 		}
-		
-		/* if((node.textContent.toLowerCase().includes("jayson tatum") || node.textContent.toLowerCase().includes("joe ingles") || node.textContent.toLowerCase().includes("harry giles") || node.textContent.toLowerCase().includes("michael jordan") || node.textContent.toLowerCase().includes("paul pierce") || node.textContent.toLowerCase().includes("dwight howard") || node.textContent.toLowerCase().includes("kevin durant") || node.textContent.toLowerCase().includes("nikola jokic") || node.textContent.toLowerCase().includes("shaun livingston") || node.textContent.toLowerCase().includes("deandre jordan") || node.textContent.toLowerCase().includes("derrick rose") || node.textContent.toLowerCase().includes("luol deng") || node.textContent.toLowerCase().includes("goran dragic") || node.textContent.toLowerCase().includes("lance stephenson") || node.textContent.toLowerCase().includes("steph curry") || node.textContent.toLowerCase().includes("wayne ellington") || node.textContent.toLowerCase().includes("javale mcGee") || node.textContent.toLowerCase().includes("tim duncan") ) && node.children.length === 0){
-
-			nodeArray.push(node);
-		} */
-	} 
+	}
 });
 
 function replaceText(arr1, arr2) { // may want to decide which array is bigger before loop and customize for performance
