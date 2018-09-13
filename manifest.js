8c91e9a5a342f6704092dc486032843b87d6eac1
Alex Richards <AlexCanCode18@gmail.com>
2018-09-13 14:45:54 -0600

    Reorganizing file structure to make more sense for an extension

---
+++ manifest.js
@@ -0,0 +1,11 @@
+{
+	"name": "Player Stats", 
+	"version": "0.1", 
+	"description": "A simply, fast way to view stats without navigating to an outside page.",
+	"permissions":["storage"],
+	"background": {
+		"scripts":["background.js"],
+		"persistent":false
+	},
+	"manifest_version": 2
+}
