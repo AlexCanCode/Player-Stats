51d6dcf4dc19eabab6b882b339544ffc10c84161
Alex Richards <arichards@go-iei.com>
2018-09-25 13:15:46 -0600

    implemented logic to populate to populate tooltip

--- main/content.js
+++ main/content.js
@@ -1,6 +1,7 @@
 let serializedPageText; 
 let playersFoundNames; //populated with sendMessage response callback
 let nodeArray = []; //Array of all nodes that contain players names
+let responseMap; //To be an object of arrays where the player name is the key for fast lookup while creating tooltip. May want to change main.js response format to render this unecessary. 
 
 //pushes all non-blank elements to an array and returns that array - need to set to run on load, perhaps? Otherwise might not get everything
  function cleanArray(arr){
@@ -42,12 +43,18 @@ function walkTheDOM(node, func) {
 
 function insertStatsAndName(match){ //used by replaceText to fill in match portion
 
-	return `<span class='stat-box' data-tippy-theme="${match}">${match}</span>`;
+	return `<span class='stat-box' data-player='${match}'>${match}</span>`;
 }
 
-function prepareStatsAndNames(arr){ //to be used to format stats to be inserted into table
-	//let playerDataArray = arr.response;
-	console.log(arr[0]); 
+function prepareStatsAndNames(obj){ 
+	let key;
+	let map = {};
+	for(i = 0; i < (obj.length - 1); i++){
+		key = obj[i].Player.toLowerCase();
+		map[key] = obj[i];
+	}
+	console.log(map);
+	return map;
 }
 
 
@@ -73,13 +80,13 @@ function init() {
 	    	return false;
 	    }
 	    else {
-		    console.log(response.response); //DEBUGGING ONLY
-		    prepareStatsAndNames(response.response);
+	    	console.log(response.response);
 		    playersFoundNames = extractNames(response.response); 
+		    responseMap = prepareStatsAndNames(response.response);
 		    //Walk the DOM and return all nodes with text that matches a name in players
 			walkTheDOM(document.body, function(node) {
 			    if(node.children){
-			        if(node.children.length === 0 || node.tagName === "P"){ //all non-parent nodes and paragraphs. Will need to teak for performance and accuracy. BUT NEED to make sure they don't conflict as sometimes it will grab both p element and the a element within it. Doesn't work for <a> tags within a paragraph
+			        if(node.children.length === 0){ //all non-parent nodes and paragraphs. Will need to tweak for performance and accuracy. BUT NEED to make sure they don't conflict as sometimes it will grab both p element and the a element within it. Doesn't work for <a> tags within a paragraph || node.tagName === "P"
 			            if(new RegExp(playersFoundNames.join("|"), "i").test(node.textContent)) {
 			                    nodeArray.push(node);
 			            };
@@ -87,11 +94,15 @@ function init() {
 			    };
 			});
 			replaceText(nodeArray, playersFoundNames); //IDEA - If nothing else works for populating the data, you could intiate a counter that keeps track of which player you are populating for. PROBLEM is it would be prone to break on things like reload and what not. 
-			tippy(document.querySelectorAll(".stat-box"), {
+			const nodeCollectionForTippy = document.querySelectorAll(".stat-box");
+			let counter = 0;
+			tippy(nodeCollectionForTippy, {
 				allowHTML: true,
 				content: function() { //Idea: Set one of tippy.js' data attribute (data-tippy-[attr]) and read it to understand what player. 
-					console.log(this);
-					return `<table>
+				const stat = responseMap[nodeCollectionForTippy[counter].dataset.player.toLowerCase()]; //gets individual stats for current player
+				counter++;
+					return `<h4>${stat.Player}</h4>
+					<table>
 			<tr>
 				<th>ppg</th>
 				<th>rpg</th>
@@ -99,17 +110,17 @@ function init() {
 				<th>per</th>
 			</tr>
 			<tr>
-				<td>21.7</td>
-				<td>3.8</td>
-				<td>6.7</td>
-				<td>19.4</td>
+				<td>${stat["PS\/G"]}</td>
+				<td>${stat.TRB}</td>
+				<td>${stat.AST}</td>
+				<td>n/a</td>
 			</tr>
 			<tr>
 				<td colspan="4"><a href="https://www.basketball-reference.com/players/c/curryst01.html">Full Stats</a></td>
 			</tr>
 			</table>`}, 
 				placement: "right", 
-				zIndex: 999999
+				zIndex: 999999, 
 			})
 		}
 	}); 
