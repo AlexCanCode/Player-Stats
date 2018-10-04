7cab1570bf1ae3aac8785a040e53f4438d633c69
Alex Richards <AlexCanCode18@gmail.com>
2018-10-04 11:14:29 -0600

    same as previous commit

--- main/content.js
+++ main/content.js
@@ -9,7 +9,7 @@ const teamColors = {"ATL": "#E03A3E", "BOS": "#007A33", "BRK": "#000000", "CHI":
 //pushes all non-blank elements to an array and returns that array - need to set to run on load, perhaps? Otherwise might not get everything
  function cleanArray(arr){
     let arrayOne = [];
-    for(var i = 0; i < arr.length; i++){
+    for(let i = 0; i < arr.length; i++){
         if(arr[i]) {
             arrayOne.push(arr[i]);
         };
@@ -44,10 +44,9 @@ function walkTheDOM(node, func) {
     };
 };
 
-function addTooltipClasses(arr){
+function findAllNodesWithPlayerNames(arr){
 	let htmlCollection = document.querySelectorAll("p, a, span, h1, h2, h3, h4, h5, h6");
 	htmlCollection.forEach(element => {
-		console.log(element);
 		if(new RegExp(arr.join("|"), "i").test(element.textContent)) {
 		    nodeArray.push(element);
 		};
@@ -61,7 +60,7 @@ function insertStatsAndName(match){ //used by replaceText to fill in match porti
 function prepareStatsAndNames(obj){ 
 	let key;
 	let map = {};
-	for(i = 0; i < (obj.length - 1); i++){
+	for(i = 0; i < obj.length; i++){
 		key = obj[i].Player.toLowerCase();
 		map[key] = obj[i];
 	}
@@ -72,8 +71,8 @@ function prepareStatsAndNames(obj){
 
 //Loop through text nodes with players names and wrap with span
 function replaceText(arr1, arr2) {  //Problem of missing players on page likely stems from this function as the node array variable has the right elements but this funciton is failing to put the right class in them. For some reason, on the test page this function works when the test paragraph is frist but not when it is followed by several other paragraphs. 
-    for(i = 0; i < (arr1.length - 1); i++){
-        for(j = 0; j < (arr2.length - 1); j++){
+    for(i = 0; i < arr1.length; i++){
+        for(j = 0; j < arr2.length; j++){
             const regex = new RegExp(arr2[j], 'ig');
             arr1[i].innerHTML = arr1[i].innerHTML.replace(regex, insertStatsAndName); 
         };
@@ -88,6 +87,7 @@ function createAndPopulateTooltips() {
 			content: function() { 
 			const stat = responseMap[nodeCollectionForTippy[counter].dataset.player.toLowerCase()]; //gets individual stats for current player
 			counter++;
+			console.log(stat);
 			let playerName = stat.Player.toLowerCase().split(" ");
 				return `<h4 id="stat-box-header-${counter}" style="background-color: ${teamColors[stat.Tm]};"><a target="_blank" href="https://www.basketball-reference.com/players/${playerName[1].charAt()}/${playerName[1]}${playerName[0].charAt()}${playerName[0].charAt(1)}01.html">${stat.Player}</a> | ${stat.Pos} ${stat.Tm}</h4>
 				<table id="stat-box-table-${counter}">
@@ -141,7 +141,7 @@ function init() {
 		    playersFoundNames = extractNames(response.response); 
 		    responseMap = prepareStatsAndNames(response.response);
 		    //Walk the DOM and return all nodes with text that matches a name in players
-		    addTooltipClasses(playersFoundNames);
+		    findAllNodesWithPlayerNames(playersFoundNames);
 			/*walkTheDOM(document.body, function(node) {
 			    if(node.children){
 			        if(acceptedTagNames.includes(node.tagName)){ //This search logic needs work. Does not pick up all instance of names on a page. Need to find a balence between performance and coverage. Will need tweaking and a deep dive into how the funciton works. 
