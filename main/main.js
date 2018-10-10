5d05ed18124380688825da8ddfe4b7b4f5ca3ebe
Alex Richards <AlexCanCode18@gmail.com>
2018-10-10 16:05:33 -0600

    added express to backend

--- main/main.js
+++ main/main.js
@@ -118,22 +118,3 @@ chrome.runtime.onMessage.addListener(
       console.log(PlayerMap.playerSearch(request, formattedStatsObjectJSON));
       sendResponse({response: (PlayerMap.playerSearch(request, formattedStatsObjectJSON))});
   }); 
-
-//Requests JSON Stat Object from Local Server for updating json object
-// let updateToday = {day: 9, updated: true};
-
-let xhr = new XMLHttpRequest();
-xhr.open("GET", "http://localhost:3000/", true);
-xhr.onreadystatechange = function() {
-  if(xhr.readyState == 4) {
-    formattedStatsObjectJSON = JSON.parse(xhr.response);
-  };
-};
-
-xhr.send();
-
-/* Next Steps
-
-1. DONE - Have the main funciton rehash the data when it receives it to account for new names (maybe put all logic in one function and rerun when receieving the data back)
-2. Determine how to measure a day's passing and make the update at that point
-*/
