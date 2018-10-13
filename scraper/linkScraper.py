28c84cf5d06f247c17200d108a6559b93c29b514
Alex Richards <AlexCanCode18@gmail.com>
2018-10-13 12:28:24 -0600

    configured URL grabber and added to webScraper.py

--- scraper/linkScraper.py
+++ scraper/linkScraper.py
@@ -13,14 +13,16 @@ list_of_rows = []
 for row in table.findAll('tr'):
 	list_of_cells = []
 	for cell in row.findAll('a'):
-                print(a)
-##		text = cell.text
-##		list_of_cells.append(text)
-##	list_of_rows.append(list_of_cells)
-##
-##clean_list_of_rows = filter(None, list_of_rows)
-##
-##outfile = open("./players.csv", "w", newline='')
-##writer = csv.writer(outfile)
-##writer.writerow(["Player", "Pos", "Age", "Tm", "G", "GS", "MP", "FG", "FGA", "FG%", "3P", "3PA", "3P%", "2P", "2PA", "2P%", "eFG%", "FT", "FTA", "FT%", "ORB", "DRB", "TRB", "AST", "STL", "BLK", "TOV", "PF", "PS/G"])
-##writer.writerows(clean_list_of_rows)
+		text = cell['href']
+		if "players" not in text:
+			continue
+		else:
+			list_of_cells.append(text)
+	list_of_rows.append(list_of_cells)
+
+clean_list_of_rows = filter(None, list_of_rows)
+
+outfile = open("./URLplayers.csv", "w", newline='')
+writer = csv.writer(outfile)
+writer.writerow(["HREF"])
+writer.writerows(clean_list_of_rows)
