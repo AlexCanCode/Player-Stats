9f37b6ca4395cdcba89e79e2e1fff8bbb1b0f10e
AlexCanCode <AlexCanCode18@gmail.com>
2018-12-25 09:50:25 -0700

    chirstmas commit - add needs for options tests

--- tests/specs/options.specs.js
+++ tests/specs/options.specs.js
@@ -1,3 +1,18 @@
-Describe("Options Script", () => {
+//Need to check the local storage object for each funciton and make sure there are no:
+// - Unintended consequences
+// - Double saves
+// - Non-saves
+// - Other odd functionality
+//Should be able to test all changes added in future 
+
+describe("Options Script", () => {
+
+	describe("save_options", () => {
+
+	})
 	
+	describe("restore_options", () => {
+
+	}
+
 })
